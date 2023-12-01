0b8af663d9aa82c928acd74a10c0b194661384d2
ROGERIO MOLINA <rogeriosmolina1973@gmail.com>
2023-11-30 23:34:23 -0300

    Realizando a soma de 2 numeros interios

--- Tester.java
+++ Tester.java
@@ -1,6 +1,8 @@
 public class Tester{
 	public static void main(String[] args){
-		System.out.println("Minha primeira versao para o gitHub");
+		System.out.println("Minha primeira versao para o gitHub!");
 		System.out.println("Mandando minha Segunda Versao para o gitHub");
+		int soma = 5 + 5;
+		System.out.println(String.format("Valor total : %d", soma));
 	}	
 }
