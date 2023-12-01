34ca10b5d2e2f898d4609e2be375f7513fd06b6f
ROGERIO MOLINA <rogeriosmolina1973@gmail.com>
2023-11-30 23:29:07 -0300

    Acrescentando mensagem para segundo commit

--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
 	public static void main(String[] args){
 		System.out.println("Minha primeira versao para o gitHub");
+		System.out.println("Mandando minha Segunda Versao para o gitHub");
 	}	
 }
